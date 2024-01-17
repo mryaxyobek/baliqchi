46e76ac7c3179ff37118a4dbeb5860fb0ea7b201
mryaxyobek <pubgn9642@gmail.com>
2024-01-17 19:26:17 +0500

    added news data

---
+++ src/assets/data.js
@@ -0,0 +1,140 @@
+import { v4 as randomId } from "uuid";
+import {
+    new1,
+    new2,
+    new3,
+    new4,
+    new5,
+    new6,
+    new7,
+    new8,
+    new9,
+} from "../assets/images";
+export const news = [
+    {
+        id: randomId(),
+        title:
+            "Abdulla Aripovga 8 ta tuman hokimining lavozimiga loyiqligini hal qilish topshirildi",
+        subTitle:
+            "Ushbu hokimlar yil davomida auksionga umuman yer chiqarmagani qayd etilgan.",
+        image: new1,
+        description: [
+            "Bosh vazirga Andijon viloyatining Baliqchi va Jalalquduq, Toshkent shahrining Bektemir, Mirobod, Olmazor, Chilonzor, Yunusobod va Yakkasaroy tumanlari hokimlarining lavozimiga loyiqligini hal qilish topshirildi. Bu haqda prezident Shavkat Mirziyoyev raisligida 2024 yilda makroiqtisodiy barqarorlik va iqtisodiy rivojlanishni ta’minlash bo‘yicha ustuvor vazifalar yuzasidan videoselektor yig‘ilishda ma’lum qilingan.",
+            "Bosh vazirga yil davomida auksionga umuman yer chiqarmagan Baliqchi, Jalaquduq, Bektemir, Mirobod, Olmazor, Chilonzor, Yunusobod, Yakkasaroy tumanlari hokimlarining lavozimiga loyiqligini hal qilish topshirildi», deyiladi xabarda.",
+            "Davaktiv agentligi yerlarni auksionda sotish bo‘yicha 4 trillion so‘mlik rejani tumanbay ishlab chiqishi va ijrosini ta’minlashi belgilandi.",
+            "Tumanning master rejasi bo‘lsa, bu juda katta mablag‘, tadbirkorlar va bandlik uchun manba bo‘ladi. Lekin hozirgacha bor-yo‘g‘i 17 ta kichik aholi punktining master rejasi tayyorlandi.",
+            "Iqtisodiy kompleks rahbariyatiga yil yakuniga qadar 70 ta, kelasi yili barcha tumanlarning master rejalarini ishlab chiqishga topshiriq berildi.",
+        ],
+        date: new Date(2024, 0, 5),
+    },
+    {
+        id: randomId(),
+        title:
+            "Makroiqtisodiy barqarorlik va iqtisodiy rivojlanishni ta’minlash bo‘yicha ustuvor vazifalar muhokama qilindi",
+        subTitle:
+            "Prezident Shavkat Mirziyoyev raisligida 16 yanvar kuni 2024 yilda makroiqtisodiy barqarorlik va iqtisodiy rivojlanishni ta’minlash bo‘yicha ustuvor vazifalar muhokamasi yuzasidan videoselektor yig‘ilishi o‘tkazildi.",
+        image: new2,
+        description: [
+            "2023 yilda mamlakatimizda iqtisodiy o‘sish 6 foizni tashkil qildi. Jumladan, sanoat 6, xizmat ko‘rsatish 6,8, qurilish 6,4, qishloq xo‘jaligi 4,1 foiz o‘sgan.",
+            "Bu yil yalpi ichki mahsulot hajmini kamida 6 foizga ko‘paytirib, 100 milliard dollarga yetkazish maqsad qilingan. Yig‘ilishda shu boradagi vazifalar, zaruriy chora-tadbirlar muhokama qilindi.",
+            "Davlatimiz rahbari buning uchun, avvalo, eng katta islohotchi bo‘lgan iqtisodiy kompleks xodimlari juda qattiq ishlashi kerakligini ta’kidladi.",
+            "Masalan, umumiy sanoat hajmi o‘sgani bilan, ishlab chiqarishda qo‘shilgan qiymat 40 foizdan oshmayapti. Bular asosan importga qaramlik, energiya iste’moli yuqoriligi va ortiqcha xarajatlar ko‘pligi sababli bo‘lmoqda.",
+        ],
+        date: new Date(2024, 0, 2),
+    },
+    {
+        id: randomId(),
+        title: "“Jinoyat omillari” moduli yaratiladi",
+        subTitle:
+            "Davlatimiz rahbarining “Kriminologiya sohasida ilmiy-amaliy tadqiqot ishlarini tashkil etish chora-tadbirlari to‘g‘risida”gi qarori qabul qilindi.",
+        image: new3,
+        description: [
+            "Qaror bilan Prezident farmoni bilan tashkil etilgan Kriminologiya tadqiqot institutining kriminologiya sohasida ilmiy-amaliy tadqiqot ishlarini tashkil etishning ustuvor yo‘nalishlari belgilab berildi.  ",
+            "Xususan, institut jinoyatchilik, shu jumladan, latent jinoyatchilik tendensiyalarini tizimli ravishda tahlil va prognoz qilish, salbiy tendensiyalarni aniqlash va ularni bartaraf etish bo‘yicha maqsadli chora-tadbirlarni ishlab chiqadi.  ",
+            "Jinoyatning asl omillarini kompleks ilmiy diagnoz asosida aniqlab, kriminologik faoliyatga sun’iy intellekt va zamonaviy raqamli texnologiyalarni joriy etib boradi.  ",
+            "Institut kriminologiya sohasi bo‘yicha tayanch ilmiy tashkilot hisoblanadi hamda kriminologik faoliyatni ekspertlik va ilmiy ta’minlash ishlarini vakolati doirasida muvofiqlashtiradi.  ",
+        ],
+        date: new Date(2024, 0, 8),
+    },
+    {
+        id: randomId(),
+        title:
+            "Ro‘yxatdan o‘tkazmasdan o‘zboshimchalik bilan qurilgan ob’ektlar foydalanishga qabul qilinmaydi",
+        subTitle:
+            "AOKAda o‘tkazilgan brifingda Qurilish va uy-joy kommunal xo‘jaligi vazirligi axborot xizmati rahbari Saidnosir Usmonovning qayd etishicha, endilikda ro‘yxatdan o‘tkazmasdan o‘zboshimchalik bilan qurilgan ob’ektlar foydalanishga qabul qilinmaydi.",
+        image: new4,
+        description: [
+            "Vazirlar Mahkamasining 2022 yil 20 apreldagi qarori bilan qurilish sohasiga oid ma’muriy Reglamentlar tasdiqlangan. Mazkur Reglamentlarda qurilish ishlarini amalga oshirishda jismoniy va yuridik shaxslar arxitektura-rejalashtirish topshirig‘ini ishlab chiqish, loyiha-smeta hujjatlarini kelishish hamda qurilish-montaj ishlarini boshlashi uchun ob’ektni ro‘yxatdan o‘tkazishlari shartligi ko‘rsatib o‘tilgan.",
+            "Biroq ayrim holatlarda shaharsozlik sub’ektlari ya’ni mulkdorlarning mazkur reglament talablarini bajarmasligi, o‘zboshimchalik bilan qurilishlar qilayotgan holatlari ham uchrab turibdi.  ",
+            "Shu kabi masalalarni hal etish, qonunosti hujjatlaridagi bo‘shliqlarni to‘ldirish maqsadida Vazirlar Mahkamasining 2023 yil 23 noyabrdagi “Shaharsozlik faoliyatida ruxsat berish tartib-taomillarini takomillashtirishga qaratilgan O‘zbekiston Respublikasi hukumatining ayrim qarorlariga o‘zgartirish va qo‘shimchalar kiritish, shuningdek, ba’zilarini o‘z kuchini yo‘qotgan deb hisoblash to‘g‘risida”gi qarori qabul qilindi.",
+        ],
+        date: new Date(2024, 0, 14),
+    },
+    {
+        id: randomId(),
+        title:
+            "Qahramon Quronboyev O‘zbekiston mahallalari uyushmasining raisi lavozimiga saylandi",
+        subTitle:
+            "O‘zbekiston Respublikasi Prezidenti huzuridagi Davlat boshqaruvi akademiyasida “O‘zbekiston mahallalari uyushmasi faoliyatini yo‘lga qo‘yish va mahallalarda boshqaruv tizimini takomillashtirish” mavzusida ta’sis konferensiyasi bo‘lib o‘tdi.",
+        image: new5,
+        description: [
+            "Unda O‘zbekiston mahallalari uyushmasini tuzish va uning hududiy bo‘linmalarini tashkil etish, uyushma Ustavi va Ramzini, hududiy bo‘linmalarining Namunaviy nizomlarini tasdiqlash, uyushmaning Nazorat-taftish komissiyasi raisi va a’zolarini saylash, shuningdek, uyushma va uning hududiy bo‘linmalari hamda mahalla faollari oldida turgan ustuvor vazifalar to‘g‘risidagi masalalar muhokama qilindi.",
+            "Anjumanda O‘zbekiston Respublikasi Oliy Majlisi Senati Raisi T.Norboyeva, O‘zbekiston Respublikasi Prezidenti maslahatchisi, Respublika mahallani qo‘llab-quvvatlash kengashi raisining birinchi o‘rinbosari G‘.Mirzayev, vazirlik va idoralar rahbarlari, respublika hamkor tashkilotlar vakillari, shuningdek, viloyatlardan 140 nafar delegat ishtirok etdi.",
+            "Ta’sis konferensiyasida O‘zbekiston Respublikasi Oliy Majlisi Senati Raisi T.Norboyeva mahalla chinakam xalq manfaatini ko‘zlaydigan, aholi muammolarini mustaqil hal eta oladigan, davlat organlari faoliyati ustidan samarali jamoatchilik nazoratini o‘rnatishga qodir yetakchi fuqarolik jamiyati institutiga aylanganini qayd etdi. Mahallaning joylarda xalqning maslakdoshi va ko‘makdoshiga, ta’bir joiz bo‘lsa, “adolat tarozisi”ga aylanishi, hech shubhasiz, odamlarning davlatga bo‘lgan ishonchini yanada mustahkamlaydi.",
+        ],
+        date: new Date(2024, 0, 4),
+    },
+    {
+        id: randomId(),
+        title:
+            "O‘zbekiston-Rossiya ilm-fan integratsiyasi: amaliy va nazariy mushtaraklik yo‘lida Sankt-Peterburgda mintaqalararo rektorlar konferensiyasi o‘tkazildi",
+        subTitle:
+            "Rossiya Federatsiyasi va O‘zbekiston Respublikasi o‘rtasidagi oliy ta’lim tizimida o‘rnatilgan institutsional hamkorlik ilm-fan va ta’lim sifatini oshirishda amaliy va nazariy mushtaraklik kasb etadi.",
+        image: new6,
+        description: [
+            "Shu maqsadda Sankt-Peterburg shahrida Rossiya federatsiyasi va O‘zbekiston Respublikasi universitetlari rektorlarining mintaqalararo konferensiyasi bo‘lib o‘tdi. Konferensiyadan ko‘zlangan asosiy maqsad — Rossiya va O‘zbekistonning ilm-fan tizimlarida o‘zaro manfaatdorlikning ijobiy dinamikasini saqlab qolish, yirik loyihalarni amalga oshirish hamda malakali kadrlar tayyorlashdir.",
+            "Tadbirda ta’lim sohasida Rossiya-O‘zbekiston hamkorligini rivojlantirishning strategik yo‘nalishlari, jumladan, iqtisodiyot, sog‘liqni saqlash, farmatsevtika va bioinjeneriya sohalarida kadrlar tayyorlash bo‘yicha universitetlararo hamkorlikni kengaytirish, ilg‘or muhandislik maktablarini yaratish bo‘yicha tajriba almashish hamda o‘qituvchilarni tayyorlash sohasidagi ilg‘or pedagogik texnologiyalarni joriy qilish kabi masalalar ko‘rildi.",
+            "Shu bilan birga, Orolbo‘yidagi qurg‘oqchilikning oldini olish uchun rossiyalik olimlar bilan qo‘shma ilmiy tadqiqotlarni kengaytirish, ayniqsa, qishloq xo‘jaligi, agrar sohada suvni tejovchi texnologiyalarni hamkorlikda joriy qilishga doir yangi chora-tadbirlar ishlab chiqildi.",
+        ],
+        date: new Date(2024, 0, 1),
+    },
+    {
+        id: randomId(),
+        title: "Rossiya qurollanish poygasiga qo‘shilmoqchi emas",
+        subTitle:
+            "Prezident Vladimir Putinning mamlakat mudofaa vazirligi, federal mahkamalar va mudofaa sanoati kompleksi rahbariyati bilan uchrashuvida aytishicha, “Rossiya qurollanish poygasiga qo‘shilishining zarurati yo‘q”.",
+        image: new7,
+        description: [
+            "“Interfaks” xabariga ko‘ra, Putin, “Bizda Rossiya manfaatlarini ishonchli himoyasi uchun zarur barcha strategik kuchlar mavjud”, deb ta’kidladi.",
+            "Uning aytishicha, dunyodagi geosiyosiy xavflarni hisobga olib Rossiya o‘zining yadroviy qurolini modernizatsiya qilishda davom etadi.",
+        ],
+        date: new Date(2024, 0, 1),
+    },
+    {
+        id: randomId(),
+        title: "O‘zbekiston o‘q otish sporti federatsiyasi rahbariyati saylandi",
+        subTitle:
+            "Milliy olimpiya qo‘mitasida O‘zbekiston o‘q otish sporti federatsiyasining navbatdagi hisobot-saylov konferensiyasi bo‘lib o‘tdi.",
+        image: new8,
+        description: [
+            "Kun tartibiga muvofiq, federatsiyaning 2022 yilgi hisoboti tinglandi. Ta’kidlanganidek, so‘nggi yillarda sezilarli o‘sishga erishilgani sportchilarning qit’a va jahon chempionatlarida qo‘lga kiritayotgan muvaffaqiyatlarida aks etayotgani bildirildi. ",
+            "Shundan so‘ng O‘zbekiston o‘q otish sporti federatsiyasi rahbariyatini saylash masalasi ko‘rib chiqildi.",
+            "Bildirilgan fikr-mulohazalardan kelib chiqib, federatsiyaning amaldagi raisi Oybek Norinboyev nomzodi tashkilot rahbarligiga, Doniyor Qodirov, Hamdam Qarshiyev, Ramiz Yenaliyev rais o‘rinbosari, Mardonbek Davronov federatsiya bosh kotibi vazifalariga saylandi.  ",
+        ],
+        date: new Date(2024, 0, 9),
+    },
+    {
+        id: randomId(),
+        title:
+            "Prezident: bog‘chalarda ta’lim va tarbiya oladigan bolalar soni 600 mingdan 2 millionga yetkazilib, qamrov ko‘lami 70 foizga chiqdi",
+        subTitle:
+            "Xabar berganimizdek, poytaxtimizda Kichik yoshdagi bolalarni tarbiyalash va ta’lim berishga bag‘ishlangan II Butunjahon konferensiyasi bo‘lib o‘tmoqda.",
+        image: new9,
+        description: [
+            "Ma’lumki, yurtimizda yaqin o‘tmishda bolalarni maktabgacha ta’lim bilan qamrab olish ko‘rsatkichi 27 foizga tushib qolgan edi. Deyarli barcha maktabgacha ta’lim tashkilotlarida zamonaviy texnologiyalar va qo‘llanmalar yo‘q, binolari ta’mirtalab edi.",
+            "Bu esa bolalarimizni to‘laqonli sifatli ta’lim va tarbiya olishini cheklagan. Holbuki, farzandlarimiz tarbiyasida eng asosiy bo‘g‘in hisoblangan maktabgacha ta’lim tizimining ijtimoiy hayotimizdagi o‘rni va ahamiyatini hech narsa bilan o‘lchab, baholab bo‘lmaydi.",
+            "–Biz islohotlarning birinchi kunlaridanoq mavjud ta’lim sohasini, xususan, maktabgacha ta’limni tubdan modernizatsiya qilishga qaror qildik.Shu maqsadda yurtimiz tarixida ilk bor alohida Maktabgacha ta’lim vazirligini tashkil etdik. Bog‘chalarni zamon talablariga mos ravishda yangi bosqichga ko‘tarish, ilg‘or tarbiya uslublari va metodikalarini keng qo‘llash, o‘quv-moddiy bazasini kuchaytirishga katta e’tibor qaratmoqdamiz, – dedi davlatimiz rahbari. –Sohani keng rivojlantirish uchun xususiy sektor kirib kelishiga ham qulay shart-sharoitlar yaratib berdik. Bog‘chalarda har bir tarbiyalanuvchi bola uchun Davlat byudjeti hisobidan subsidiyalar berila boshlandi.",
+        ],
+        date: new Date(2024, 0, 6),
+    },
+];
