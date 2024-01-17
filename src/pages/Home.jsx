734a5ba3b06d7706f9e01942fe62342e8ac4215d
mryaxyobek <pubgn9642@gmail.com>
2024-01-17 19:26:55 +0500

    added homepage hero

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Hero from '../components/HomePageHero';
+import LatestNews from '../components/LatestNews';
 
 const Home = () => {
     return (
         <>
             <Hero />
+            <LatestNews/>
         </>
     )
 };
